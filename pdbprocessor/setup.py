4df93f247b91fcfa1f4607c7faf93b0a1c8b5129
igik20 <53370556+igik20@users.noreply.github.com>
2021-11-17 12:42:33 +0100

    Update setup.py

--- pdbprocessor/setup.py
+++ pdbprocessor/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(
     name='pdbprocessor',
-    version='0.0.2',
+    version='0.2.0',
     entry_points={
         'console_scripts': [
             'pdbprocessor=pdbprocessor:main'
